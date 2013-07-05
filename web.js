a34dbc5d7e81b59c86377de91e5086b013222e28
Bartek Glowacki <marathone@gmail.com>
2013-07-05 13:50:07 +0000

    reading from index.html

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
 
+var fs = require('fs');
+
+var bufferedText = fs.readFile('index.html', function (err, data) {
+  return data.toString('utf-8');
+});
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(bufferedText);
 });
 
 var port = process.env.PORT || 5000;
